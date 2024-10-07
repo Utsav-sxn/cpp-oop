bb8f2713cfca7e1560f8b855f70ce3ce7077cf49
Utsav <utsavmra2004@gmail.com>
2024-10-07 12:54:07 +0530

    Update constructDestruct.cpp

--- constructDestruct.cpp
+++ constructDestruct.cpp
@@ -16,5 +16,5 @@ int main(){
         std::unique_ptr<MyClass>ptr  = std::make_unique<MyClass>();
         //destructor invoked
     }
-    system("pause>nul");//idk what is this s**t,seems like pressing a key to continue in the terminal
+    system("pause>nul");//seems like pressing a key to continue in the terminal
 }
